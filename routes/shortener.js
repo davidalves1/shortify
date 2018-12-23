640f8c4e29a1ecda87124984a761a759c63df2d7
davidalves1 <alves.david@outlook.com>
2018-12-22 23:49:25 -0200

    Store URL and return the shortener link

--- routes/shortener.js
+++ routes/shortener.js
@@ -1,13 +1,16 @@
 const Shortener = require('../models/shortener');
 const db = require('../config/database');
+const config = require('../config/index');
+const randomHash = require('random-hash');
 
 module.exports = server => {
   server.get('/:hash', async (req, res, next) => {
     try{
+      console.log('BUSCANDO URL');
       Shortener.findOne({code: req.params.hash}, (err, url) => {
         if (err || url === null) {
           console.error('api error', err);
-          res.send(err);
+          res.redirect(301, config.base_url);
           return;
         }
 
@@ -16,6 +19,41 @@ module.exports = server => {
     } catch (err) {
       res.status(500);
       return res.json({error: 'Deu erro'});
+    }
+  });
+
+  server.post('/shortener', async (req, res) => {
+    try {
+      const hash = randomHash.generateHash({ length: 6 });
+      const shorten = `${config.base_url}/${hash}`;
+
+      const found = await Shortener.findOne({ original: req.body.url });
+
+      if (found) {
+        return res.json(found);
+      }
+
+      const data = {
+        code: hash,
+        original: req.body.url,
+        shorten
+      };
+
+      const shortener = new Shortener(data);
+      shortener.save((err, id) => {
+        console.log({ id });
+
+        if (err) {
+          console.log('DEU ERRO NO CADASTRO');
+          res.status(500);
+          return res.json({ err });
+        }
+
+        return res.json({ shorten });
+      });
+    } catch (err) {
+      res.status(500);
+      return res.json({err});
     }
   });
 }
